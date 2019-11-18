e5d5472ef89a6834eda7e6492aa28b97e906fc79
jamievullo <jvullo@protonmail.com>
2019-11-18 08:23:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+
+let breeds = []
+
+document.addEventListener("DOMContentLoaded", function {
+    fetchImages();
+
+})
+
+function fetchImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(function(response) {
+        return response.json();
+    }).then(results => {
+        results.message.forEach(image => addImage(image))
+});
+}
+
+
+const dogImage = document.getElementById("dog-image-container")
