69ca4c8563ac9b125a47586202ce99f43b20cb1e
ashok997 <ashok997@gmail.com>
2019-12-04 18:51:37 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
-console.log('%c HI', 'color: firebrick')
+function fetchDogs() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(resp => resp.json())
+        .then(json => renderDogs(json))
+}
+
+function renderDogs(json) {
+    const div = document.querySelector("#dog-image-container")
+    console.log(json)
+    json.message.forEach(dog => {
+        const image = document.createElement("img")
+        image.src = dog
+        div.appendChild(image)
+    })
+}
+
+function fetchBreeds() {
+    return fetch("https://dog.ceo/api/breeds/list/all")
+        .then(resp => resp.json())
+        .then(json => renderBreeds(json))
+}
+
+function renderBreeds(json) {
+    let allBreeds = [];
+    let i = 0;
+
+    const ul = document.querySelector("#dog-breeds")
+    for (const breed in json.message) {
+        allBreeds[i] = breed
+        const li = document.createElement("li")
+        li.id = breed
+        li.innerText = breed
+        ul.appendChild(li)
+        i++
+        li.addEventListener("click", function () {
+            this.style.color = "red";
+        });
+    }
+
+    const breedSelect = document.querySelector("#breed-dropdown")
+    breedSelect.addEventListener("change", function (event) {
+        const selection = event.target.value
+        ul.innerHTML = "";
+        let filteredBreed = allBreeds.filter(function (breed) {
+            return breed[0] === selection
+        });
+
+        filteredBreed.forEach(breed => {
+            const li = document.createElement("li")
+            li.id = breed
+            li.innerText = breed
+            ul.appendChild(li)
+        })
+    })
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetchDogs()
+    fetchBreeds()
+    // const breedSelect = document.querySelector("#breed-dropdown")
+    // breedSelect.addEventListener("change", function (event) {
+    //     const selection = event.target.value
+    // })
+})
